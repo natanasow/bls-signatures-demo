c547831a0ed62be8a612ff6038381b8033eb249a
nikolay <n.atanasow94@gmail.com>
2024-10-23 23:54:28 +0300

    chore: activate all tests
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

--- test/blsVerifier.spec.ts
+++ test/blsVerifier.spec.ts
@@ -72,69 +72,69 @@ describe('BLS verifier', function () {
         expect(percentageDiff).to.be.lessThanOrEqual(MAX_PERCENTAGE_DIFFERENCE);
     });
 
-    // for (const actors of [5, 15, 25, 50, 100, 150, 200]) {
-    //     let g1PubKeyCallData: Array<BigInt[]>;
-    //     let g1SigAndMsgCallData: Array<BigInt[]>;
-    //
-    //     it(`single verification using G1 for ${actors} aggregated public key and G2 for ${actors} aggregated signature and same message`, async () => {
-    //         let pubKeysG1Aggregated: mcl.G1;
-    //         let sigG2Aggregated: mcl.G2;
-    //
-    //         const msgG2: mcl.G2 = blsBn254Helper.g2FromHex(ethers.keccak256('0x160c'));
-    //         for (let i = 0; i < actors; i++) {
-    //             const signer: KeyPairG1 = blsBn254Helper.createKeyPairG1PubKey();
-    //             const pubKeyG1: mcl.G1 = signer.publicKeyG1;
-    //             const sigG2: mcl.G2 = blsBn254Helper.signG2(msgG2, signer.secretKey);
-    //
-    //             // @ts-ignore
-    //             pubKeysG1Aggregated = (i === 0) ? pubKeyG1 : blsBn254Helper.pAdd(pubKeysG1Aggregated, pubKeyG1);
-    //             // @ts-ignore
-    //             sigG2Aggregated = (i === 0) ? sigG2 : blsBn254Helper.pAdd(sigG2Aggregated, sigG2);
-    //         }
-    //
-    //         g1PubKeyCallData = [
-    //             blsBn254Helper.serializeG1Point(pubKeysG1Aggregated),
-    //             blsBn254Helper.serializeG2Point(msgG2),
-    //             blsBn254Helper.serializeG2Point(sigG2Aggregated)
-    //         ]
-    //         const isEcPairingValid: boolean = await contract.verifySingleG1PubKeyG2SigAndMsg(...g1PubKeyCallData);
-    //         expect(isEcPairingValid).to.be.true;
-    //     });
-    //
-    //     it(`single verification using G1 for ${actors} signature and same message and G2 for ${actors} public key`, async () => {
-    //         let pubKeysG2Aggregated: mcl.G2;
-    //         let sigG1Aggregated: mcl.G1;
-    //
-    //         const msgG1 = blsBn254Helper.g1FromHex(ethers.keccak256('0x160c'));
-    //         for (let i = 0; i < actors; i++) {
-    //             const signer: KeyPairG2 = blsBn254Helper.createKeyPairG2PubKey();
-    //             const pubKeyG2: mcl.G2 = signer.publicKeyG2;
-    //             const sigG1: mcl.G1 = blsBn254Helper.signG1(msgG1, signer.secretKey);
-    //
-    //             // @ts-ignore
-    //             pubKeysG2Aggregated = (i === 0) ? pubKeyG2 : blsBn254Helper.pAdd(pubKeysG2Aggregated, pubKeyG2);
-    //             // @ts-ignore
-    //             sigG1Aggregated = (i === 0) ? sigG1 : blsBn254Helper.pAdd(sigG1Aggregated, sigG1);
-    //         }
-    //
-    //         g1SigAndMsgCallData = [
-    //             blsBn254Helper.serializeG2Point(pubKeysG2Aggregated),
-    //             blsBn254Helper.serializeG1Point(msgG1),
-    //             blsBn254Helper.serializeG1Point(sigG1Aggregated)
-    //         ];
-    //
-    //         const isEcPairingValid: boolean = await contract.verifySingleG1SigAndMsgG2PubKey(...g1SigAndMsgCallData);
-    //         expect(isEcPairingValid).to.be.true;
-    //     });
-    //
-    //     it(`gas estimation for ${actors} aggregated signatures and public keys should be within a range`, async () => {
-    //         const pubKeyG1Gas: BigInt = await contract.verifySingleG1PubKeyG2SigAndMsg.estimateGas(...g1PubKeyCallData);
-    //         const sigAndMsgG1Gas: BigInt = await contract.verifySingleG1SigAndMsgG2PubKey.estimateGas(...g1SigAndMsgCallData);
-    //
-    //         const percentageDiff: number = 100 * Math.abs((Number(pubKeyG1Gas) - Number(sigAndMsgG1Gas)) / ((Number(pubKeyG1Gas) + Number(sigAndMsgG1Gas)) / 2));
-    //         expect(percentageDiff).to.be.lessThanOrEqual(MAX_PERCENTAGE_DIFFERENCE);
-    //     });
-    // }
+    for (const actors of [5, 15, 25, 50, 100, 150, 200]) {
+        let g1PubKeyCallData: Array<BigInt[]>;
+        let g1SigAndMsgCallData: Array<BigInt[]>;
+
+        it(`single verification using G1 for ${actors} aggregated public key and G2 for ${actors} aggregated signature and same message`, async () => {
+            let pubKeysG1Aggregated: mcl.G1;
+            let sigG2Aggregated: mcl.G2;
+
+            const msgG2: mcl.G2 = blsBn254Helper.g2FromHex(ethers.keccak256('0x160c'));
+            for (let i = 0; i < actors; i++) {
+                const signer: KeyPairG1 = blsBn254Helper.createKeyPairG1PubKey();
+                const pubKeyG1: mcl.G1 = signer.publicKeyG1;
+                const sigG2: mcl.G2 = blsBn254Helper.signG2(msgG2, signer.secretKey);
+
+                // @ts-ignore
+                pubKeysG1Aggregated = (i === 0) ? pubKeyG1 : blsBn254Helper.pAdd(pubKeysG1Aggregated, pubKeyG1);
+                // @ts-ignore
+                sigG2Aggregated = (i === 0) ? sigG2 : blsBn254Helper.pAdd(sigG2Aggregated, sigG2);
+            }
+
+            g1PubKeyCallData = [
+                blsBn254Helper.serializeG1Point(pubKeysG1Aggregated),
+                blsBn254Helper.serializeG2Point(msgG2),
+                blsBn254Helper.serializeG2Point(sigG2Aggregated)
+            ]
+            const isEcPairingValid: boolean = await contract.verifySingleG1PubKeyG2SigAndMsg(...g1PubKeyCallData);
+            expect(isEcPairingValid).to.be.true;
+        });
+
+        it(`single verification using G1 for ${actors} signature and same message and G2 for ${actors} public key`, async () => {
+            let pubKeysG2Aggregated: mcl.G2;
+            let sigG1Aggregated: mcl.G1;
+
+            const msgG1 = blsBn254Helper.g1FromHex(ethers.keccak256('0x160c'));
+            for (let i = 0; i < actors; i++) {
+                const signer: KeyPairG2 = blsBn254Helper.createKeyPairG2PubKey();
+                const pubKeyG2: mcl.G2 = signer.publicKeyG2;
+                const sigG1: mcl.G1 = blsBn254Helper.signG1(msgG1, signer.secretKey);
+
+                // @ts-ignore
+                pubKeysG2Aggregated = (i === 0) ? pubKeyG2 : blsBn254Helper.pAdd(pubKeysG2Aggregated, pubKeyG2);
+                // @ts-ignore
+                sigG1Aggregated = (i === 0) ? sigG1 : blsBn254Helper.pAdd(sigG1Aggregated, sigG1);
+            }
+
+            g1SigAndMsgCallData = [
+                blsBn254Helper.serializeG2Point(pubKeysG2Aggregated),
+                blsBn254Helper.serializeG1Point(msgG1),
+                blsBn254Helper.serializeG1Point(sigG1Aggregated)
+            ];
+
+            const isEcPairingValid: boolean = await contract.verifySingleG1SigAndMsgG2PubKey(...g1SigAndMsgCallData);
+            expect(isEcPairingValid).to.be.true;
+        });
+
+        it(`gas estimation for ${actors} aggregated signatures and public keys should be within a range`, async () => {
+            const pubKeyG1Gas: BigInt = await contract.verifySingleG1PubKeyG2SigAndMsg.estimateGas(...g1PubKeyCallData);
+            const sigAndMsgG1Gas: BigInt = await contract.verifySingleG1SigAndMsgG2PubKey.estimateGas(...g1SigAndMsgCallData);
+
+            const percentageDiff: number = 100 * Math.abs((Number(pubKeyG1Gas) - Number(sigAndMsgG1Gas)) / ((Number(pubKeyG1Gas) + Number(sigAndMsgG1Gas)) / 2));
+            expect(percentageDiff).to.be.lessThanOrEqual(MAX_PERCENTAGE_DIFFERENCE);
+        });
+    }
 
     for (const pairs of [2, 10, 20, 40, 60, 80]) {
         let g1PubKeyCallData: Array<BigInt[] | Array<BigInt[]>>;
